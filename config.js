ca8dee7cbbb62b95f70f199a8692ac9e382bbc71
JON RIO MUÑOZ <jonrio11@gmail.com>
2026-07-24 08:54:14 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
 // Consíguelo gratis en: https://www.mapillary.com/dashboard/developers
 // Es un token de cliente, está pensado para usarse en el navegador (no es secreto),
 // así que no pasa nada porque quede en el código público del repo.
-const MAPILLARY_TOKEN = "PEGA_AQUI_TU_TOKEN_MLY";
+const MAPILLARY_TOKEN = "MLY|26846645948342864|8fcbe73a76c521f4048c1b566650a3ba";
